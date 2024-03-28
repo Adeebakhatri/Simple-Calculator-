9d182e2975fc35eacb0c862802bd609075f817cd
AdeebaKhatri <160527402+Adeebakhatri@users.noreply.github.com>
2024-03-28 13:24:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+//SIMPLE CALCULATOR
+import inquirer from 'inquirer';
+//Printing a welcome message
+console.log('\n\t Welcome - CLI Simple Calculator');
+// Asking Questions From The Users Through Inquirer
+const calculator = await inquirer.prompt([
+    { message: 'Enter First Number', type: 'number', name: 'fristNumber' },
+    { message: 'Enter Second Number', type: 'number', name: 'secondNumber' },
+    {
+        message: "Select one Operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division'],
+    },
+]);
+console.log(calculator);
+//conditional Test
+if (calculator.operator === 'Addition') {
+    console.log(calculator.firstNumber + calculator.secondNumber);
+}
+else if (calculator.operator === 'Subtraction') {
+    console.log(calculator.firstNumber - calculator.secondNumber);
+}
+else if (calculator.operator === 'Multiplication') {
+    console.log(calculator.firstNumber * calculator.secondNumber);
+}
+else if (calculator.operator === 'Division') {
+    console.log(calculator.firstNumber / calculator.secondNumber);
+}
